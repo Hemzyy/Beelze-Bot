46e295be5081475f5773781cbd37cfa2de56a92f
Hamza Krika <63921551+Hemzyy@users.noreply.github.com>
2020-05-12 18:13:49 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ module.exports = {
 	async execute(message, args) {
 		const embed = {
 			title: 'invite',
-			url: 'https://discordapp.com/oauth2/authorize?client_id=700119467295768576&permissions=36826183&scope=bot'
+			url: 'https://discord.com/api/oauth2/authorize?client_id=700119467295768576&permissions=0&scope=bot'
 		};
 		message.channel.send('Click below to invite Beelze Bot to a server: ');
 		message.channel.send({embed: embed});
